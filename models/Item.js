a7a2f099ac54f87428387fdda2fcbb7ab436c6f7
shastrikapatel <shastrikapatel24@gmail.com>
2025-08-28 16:02:00 +0530

    new

--- models/Item.js
+++ models/Item.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const itemSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    price: { type: Number, required: true }
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true }
 });
 
 module.exports = mongoose.models.Item || mongoose.model("Item", itemSchema);
